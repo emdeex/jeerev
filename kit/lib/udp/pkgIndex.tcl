4df9f3640898de3fb9dfee7347d0f00401a7b43a
Jean-Claude Wippler <jc@wippler.nl>
2011-08-20 01:04:38 +0200

    add ARM build of udp

--- kit/lib/udp/pkgIndex.tcl
+++ kit/lib/udp/pkgIndex.tcl
@@ -13,4 +13,7 @@ switch [platform::generic] {
   linux-x86_64 {
     package ifneeded udp 1.0.9 [list load [file join $dir libudp1.0.9-x64.so]]
   }
+  linux-arm {
+    package ifneeded udp 1.0.9 [list load [file join $dir libudp1.0.9-arm.so]]
+  }
 }
