864ca83f9a41358f98438a4e5dd37568f5e213d5
Jean-Claude Wippler <jc@wippler.nl>
2011-09-02 17:21:17 +0200

    different bucket groups, don't keep files open

--- kit/rigs3/History.tcl
+++ kit/rigs3/History.tcl
@@ -10,11 +10,9 @@ proc APP.READY {} {
   variable fd [open [Stored path history.log] a+]
   chan configure $fd -buffering none
   
-  DefineBucket 15s 10m
-  DefineBucket 1m 1h
-  DefineBucket 5m 12h
-  # DefineBucket 1m 2d
-  # DefineBucket 1h 3y
+  DefineBuckets test:* 10m/15s 1h/1m 12h/5m
+  DefineBuckets reading:* 2d/1m 1w/5m 3y/1h
+  DefineBuckets sysinfo:* 1w/5m
 
   State subscribe * [namespace which StateChanged]
 }
@@ -38,18 +36,15 @@ proc LogValue {param value time} {
   set lastTime $time
 }
 
-proc DefineBucket {step range} {
-  set step [Ju asSeconds $step]
-  set range [Ju asSeconds $range]
-  
+proc DefineBuckets {pattern args} {
   variable buckets
-  set buckets($step) $range
-  
-  puts " step $step range $range count [/ $range $step]"
-  
-  variable minStep
-  if {$step < [Ju get minStep 9999]} {
-    set minStep $step
+  puts "  $pattern"
+  foreach x $args {
+    lassign [split $x /] range step
+    set step [Ju asSeconds $step]
+    set range [Ju asSeconds $range]
+    dict lappend buckets($pattern) $step $range
+    puts "    step $step range $range count [/ $range $step]"
   }
 }
 
@@ -57,10 +52,9 @@ proc CollectValue {param value time} {
   # Collect and aggregate values according to the different buckets we have.
   upvar [namespace current]::collector($param) myData
   if {![info exists myData]} {
-    SetupCollector $param 0 9999
+    SetupCollector $param 0 [<< 1 100] ;# -999999999 999999999
   }
-  variable minStep
-  set slot [/ $time $minStep]
+  set slot [/ $time [dict get $myData minStep]]
   if {$slot ne [dict get $myData slot]} {
     FlushSlot $param
     dict set myData slot $slot
@@ -71,35 +65,46 @@ proc CollectValue {param value time} {
 proc SetupCollector {param low high} {
   variable buckets
   upvar [namespace current]::collector($param) myData
-  set myData {chain {} slot -1 values {}}
-  # set up buckets in decreasing step size
-  set prevStep ""
-  set prevObj ""
-  foreach step [lsort -dec -int [array name buckets]] {
-    Ju assert {$prevStep eq "" ||
-                $prevStep % $step == 0 && $prevStep / $step >= 3}
-    set obj [Bucket new $param $step $buckets($step) $low $high $prevObj]
-    set prevStep $step
-    set prevObj $obj
+  set myData {minStep 9999 chain {} slot -1 values {}}
+  # figure out which buckets to set up for this parameter
+  foreach {pattern bucketlist} [array get buckets] {
+    if {[string match $pattern $param]} {
+      # set up buckets in decreasing step size
+      set prevStep ""
+      set prevObj ""
+      foreach {s r} [lsort -stride 2 -dec -int $bucketlist] {
+        if {$s < [dict get $myData minStep]} {
+          dict set myData minStep $s
+        }
+        # each larger step must be an exact multiple of the previous one
+        Ju assert {$prevStep eq "" || $prevStep % $s == 0 && $prevStep > $s}
+        set obj [Bucket new $param $s $r $low $high $prevObj]
+        set prevStep $s
+        set prevObj $obj
+      }
+      dict set myData chain $obj
+      return
+    }
   }
-  dict set myData chain $obj
 }
 
 proc FlushSlot {param} {
-  variable minStep
   upvar [namespace current]::collector($param) myData
   set slot [dict get $myData slot]
   set values [dict get $myData values]
   if {[llength $values] > 0} {
-    [dict get $myData chain] aggregate [* $slot $minStep] $values
-    #TODO propagate down (needs bucket select code)
+    set chain [dict get $myData chain]
+    if {$chain ne ""} {
+      $chain aggregate [* $slot [dict get $myData minStep]] $values
+      #TODO propagate down (needs bucket select code)
+    }
   }
   dict set myData values {}
 }
 
 Ju classDef Bucket {
   variable param step range count child type filler width \
-            prevSecs fname bfd currSlot
+            prevSecs fname currSlot
   
   constructor {bparam bstep brange blow bhigh bchild} {
     set param $bparam
@@ -112,21 +117,21 @@ Ju classDef Bucket {
     set prevSecs 0
     file mkdir [Stored path hist-bin]
     set fname [Stored path hist-bin/$id-$step-$count-$type]
-    set bfd [open $fname a+]
-    chan configure $bfd -translation binary -buffering none
-    if {[file size $fname] == 0} {
-      set filler [binary format $type 0 0 0 0]
-      set emptySlots [string repeat $filler $count]
-      # don't go back in time more than the total range we're storing
-      set firstSlot [/ [- [clock seconds] $range] $step]
-      append emptySlots [binary format cI -1 $firstSlot]
-      chan puts -nonewline $bfd $emptySlots
+    if {![file exists $fname]} {
+      my CreateFile
     }
     my RecoverSlot
   }
   
-  destructor {
-    chan close $bfd
+  method CreateFile {} {
+    set fd [my Open a+]
+    # don't go back in time more than the total range we're storing
+    set firstSlot [/ [- [clock seconds] $range] $step]
+    set filler [binary format $type 0 0 0 0]
+    set emptySlots [string repeat $filler $count]
+    append emptySlots [binary format cI -1 $firstSlot]
+    chan puts -nonewline $fd $emptySlots
+    chan close $fd
   }
   
   method SetType {low high} {
@@ -134,7 +139,7 @@ Ju classDef Bucket {
       set type crrr
     } else {
       set type cqqq
-      foreach {b t} { 7 cccs 15 cssi 23 ciii 31 ciiw 63 cwwq } {
+      foreach {b t} { 7 cccs 15 cssi 31 ciiw 63 cwwq } {
         set limit [<< 1 $b]
         if {-$limit < $low && $high < $limit} {
           set type $t
@@ -160,7 +165,7 @@ Ju classDef Bucket {
     set min [min {*}$values]
     set max [max {*}$values]
     set sum [+ {*}$values]
-    puts " : $num $min $max $sum - $values"
+    puts " : $num $min $max $sum ($param)"
     binary format ${type}cI $num $min $max $sum -1 $slot
   }
   
@@ -168,16 +173,24 @@ Ju classDef Bucket {
     # Aggregate and store new values into the proper slot.
     set slot [/ $secs $step]
     Ju assert {$slot >= $currSlot}
+    set fd [my Open]
     # don't leave gaps, fill empty slots between last one and new one
     while {$slot != $currSlot} {
-      my Store [incr currSlot] $filler
+      my Store $fd [incr currSlot] $filler
     }
-    my Store $slot [my ToBinary $slot $values]
+    my Store $fd $slot [my ToBinary $slot $values]
+    chan close $fd
   }
   
-  method Store {slot bytes} {
-    chan seek $bfd [* [% $slot $count] $width]
-    chan puts -nonewline $bfd $bytes
+  method Open {{mode r+}} {
+    set fd [open $fname $mode]
+    chan configure $fd -translation binary -buffering none
+    return $fd
+  }
+  
+  method Store {fd slot bytes} {
+    chan seek $fd [* [% $slot $count] $width]
+    chan puts -nonewline $fd $bytes
     set secs [* $slot $step]
     if {$child ne "" && $prevSecs != 0 && ![$child sameSlot $secs $prevSecs]} {
       my SendToChild $prevSecs
@@ -186,17 +199,19 @@ Ju classDef Bucket {
   }
   
   method RecoverSlot {} {
-    chan seek $bfd 0
+    set fd [my Open r]
+    chan seek $fd 0
     while true {
-      set bytes [read $bfd $width]
+      set bytes [read $fd $width]
       Ju assert {[string length $bytes] >= 5}
       binary scan $bytes cI tag slot
       if {$tag == -1} {
         puts "recovered: [clock format [* $slot $step]] ($fname)"
         set currSlot $slot
-        return
+        break
       }
     }
+    chan close $fd
   }
   
   method SendToChild {secs} {
