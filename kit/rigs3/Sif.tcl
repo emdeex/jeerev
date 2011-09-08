2a267e0c54d838f6e92dca91bb7a986c831f285e
Jean-Claude Wippler <jc@wippler.nl>
2011-09-09 01:06:16 +0200

    simplify and add comments

--- kit/rigs3/Sif.tcl
+++ kit/rigs3/Sif.tcl
@@ -55,21 +55,24 @@ proc HtmlElements {abbrev {content ""}} {
   # Take the abbreviations on one line and turn them into HTML elements.
   set openTags {}
   set closeTags {}
+  # iterate over groups separated by >'s
   foreach x [split $abbrev >] {
+    if {$x eq ""} continue
     if {[string index $x 0] in {. #}} {
-      set x "div$x"
+      set x "div$x" ;# div is implied if line starts with . or #
     }
     set tag ""
     set id ""
     set classes {}
-    set attrs {}
+    # it takes a bit of work to extract attributes of the form "/name=..."
+    # build a reverse list, so we can deal with slashes inside values
+    set attRev {}
     foreach a [lreverse [regexp -all -inline -indices {/\w+=} $x]] {
       lassign $a from to
-      set k [string range $x $from+1 $to-1]
-      set v [string range $x $to+1 end]
-      set attrs [linsert $attrs 0 $k $v]
+      lappend attRev [string range $x $to+1 end] [string range $x $from+1 $to-1]
       set x [string range $x 0 $from-1]
     }
+    # no go through each of the elements in this group
     foreach {- y} [regexp -all -inline {([#\.]?[\w\$]+)} $x] {
       switch [string index $y 0] {
         \#      { set id [string range $y 1 end] }
@@ -77,26 +80,35 @@ proc HtmlElements {abbrev {content ""}} {
         default { set tag $y}
       }
     }
-    lappend closeTags $tag
-    if {$id ne ""} {
-      append tag " id='" $id "'"
-    }
+    # if we collected any classes, create an attribute for it
     if {[llength $classes]} {
-      append tag " class='" $classes "'"
+      lappend attRev $classes class
     }
-    foreach {k v} $attrs {
+    # likewise, if we collected an id, create an attribute for that too
+    if {$id ne ""} {
+      lappend attRev $id id
+    }
+    # closing tags do not contain the attributes
+    lappend closeTags $tag
+    # now add all atributes, reversing the list back to normal first
+    foreach {k v} [lreverse $attRev] {
       append tag " " $k "='" $v "'"
     }
+    # finally, we can add an item with the element plus its attributes
     lappend openTags $tag
   }
+  # if there is no content, generate a self closing tag, i.e. "<blah ... />"
   if {$content eq "" && [llength $openTags] == 1} {
     return [list "<[lindex $openTags 0] />"]
   }
+  # opening elements are in original order, closing elements must be reversed
   set open <[join $openTags ><]>
   set close </[join [lreverse $closeTags] ></]>
+  # no content has only an opeming item, i.e. "<blah ...></blah>"
   if {$content eq ""} {
     return [list "$open$close"]
   }
+  # with content we return three items, i.e. "<blah ...>" "..." "</blah>"
   #TODO i18n
   # list $open "\[: [list [string trim $content]]]" $close
   list $open [string trim $content] $close
