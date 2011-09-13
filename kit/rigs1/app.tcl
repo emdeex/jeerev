a97381170b08325c1b3deb87a3cdbbdf92a03dd9
Jean-Claude Wippler <jc@wippler.nl>
2011-09-13 15:22:23 +0200

    add cache for loaded rigs

--- kit/rigs1/app.tcl
+++ kit/rigs1/app.tcl
@@ -105,30 +105,30 @@ proc fail {msg {cleanup 0}} {
   }
 }
 
+Ju cachedVar loadedRigs . {
+  # Used to search for hook procs in all currently loaded rigs.
+  set loadedRigs {}
+  foreach {k v} [array get ::auto_index] {
+    if {[string match ::Jm::loadRig* $v]} {
+      lappend loadedRigs {*}[info commands ::$k]
+    }
+  }
+}
+
 proc hook {hook args} {
   # Apply hooks in each loaded rig that defines it.
   # hook: name of the hook proc
   # args: arguments to pass to the hook proc
   # Returns a dict: each result is added with the hook proc path as key.
+  variable loadedRigs
   set results {}
-  foreach path [PathsOfLoadedRigs] {
+  foreach path $loadedRigs {
     set cmd ${path}::$hook
     if {[llength [info commands $cmd]] > 0} {
       dict set results $path [uplevel $cmd $args]
     }
   }
   return $results
-}
-
-proc PathsOfLoadedRigs {} {
-  # Used to search for hook procs in all currently loaded rigs.
-  set paths {}
-  foreach {k v} [array get ::auto_index] {
-    if {[string match ::Jm::loadRig* $v]} {
-      lappend paths {*}[info commands ::$k]
-    }
-  }
-  return $paths
 }
 
 proc Heartbeat {} {
