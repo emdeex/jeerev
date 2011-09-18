c33ecbad972db997047e7ec35ee0efb077dae6e3
Jean-Claude Wippler <jc@wippler.nl>
2011-09-18 10:54:20 +0200

    one last web logging tweak

--- kit/rigs2/Webserver.tcl
+++ kit/rigs2/Webserver.tcl
@@ -81,7 +81,8 @@ proc dispatch {state} {
   variable routes ;# cachedVar, see below
   set path [dict get $state request uri]
   regsub {\?.*} $path {} path
-  Log web {path $path from [dict get? [state] request header x-forwarded-for]}
+  Log web {path $path ip [dict get? [state] request peerhost]\
+                      remote [dict get? [state] request header x-forwarded-for]}
   foreach {re match} $routes {
     set args [regexp -inline $re $path]
     if {$args ne ""} {
