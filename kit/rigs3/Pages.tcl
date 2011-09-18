ddd5a9e2bc2b87f8d0f60e5032e5f45aa78f12fc
Jean-Claude Wippler <jc@wippler.nl>
2011-09-18 21:44:20 +0200

    new page expansion code, works but very messy

--- kit/rigs3/Pages.tcl
+++ kit/rigs3/Pages.tcl
@@ -8,7 +8,19 @@ proc load {path} {
   Jm autoLoader $path * Pages::
 }
 
-proc fSelect {var label args} {
+proc request {page args} {
+  set vars [dict get $::main::pages ::Pages::$page]
+  Request new $page [uplevel namespace current] $vars $args
+}
+
+proc pageVars {info} {
+  uplevel [list variable info $info]
+  #TODO this is an awkward way to do things, try to improve on it
+  #  the benefit of hooks + cachedVar is automatic cleanup (but more overhead)
+  uplevel { proc PAGES.VARS {} { variable info; return $info } }
+}
+
+proc pSelect {var label args} {
   Webserver expand [Sif html {
     label/for=s_$var: $label
     select/id=s_$var/data-bind=value:$var
@@ -16,7 +28,7 @@ proc fSelect {var label args} {
         option/value=[incr seq]: $x
   }]
 }
-proc fRadio {var label args} {
+proc pRadio {var label args} {
   Webserver expand [Sif html {
     : $label
     % foreach x $args
@@ -25,7 +37,7 @@ proc fRadio {var label args} {
       label/for=$tag: $x
   }]
 }
-proc fCheckbox {args} {
+proc pCheckbox {args} {
   Webserver expand [Sif html {
     % foreach {var label} $args
       % if {$var ne [lindex $args 0]}
@@ -34,10 +46,54 @@ proc fCheckbox {args} {
       label/for=c_$var: $label
   }]
 }
-proc fText {var label} {
+proc pText {var label} {
   Webserver expand [Sif html {
     label/for=t_$var: $label
     % set akd afterkeydown
     input/type=text/id=t_$var/size=4/data-bind=value:$var,valueUpdate:"$akd"
   }]
+}
+
+Ju classDef Request {
+  variable info
+  
+  constructor {page ns vars extra} {
+    Ju extendNamespacePath $ns ::Pages::$page ::Pages
+    set info [dict merge [Webserver state] $vars $extra]
+    dict set info page $page
+    # set up all trace variables $X(...)
+    foreach avar [my types] {
+      my variable $avar
+      array set $avar {}
+      trace add variable $avar read [callBack Tracer]
+    }
+  }
+  
+  method types {} {
+    # Returns the list of all method names matching a single uppercase letter.
+    lsearch -inline -all [info object methods [self] -all -private] {[A-Z]}
+  }
+  
+  method V {x} { return $x }
+  method X {x} { ne $x "" }
+  method H {x} { wibble enhtml $x }
+  method A {x} { wibble enattr $x }
+  method P {x} { wibble enpre $x }
+  method Q {x} { wibble enquery $x }
+
+  method Tracer {a e op} {
+    upvar ${a}($e) v
+    if {![info exists v]} {
+      # expand a:b:c notation into nested dict access
+      set path [string map {: " "} $e]
+      # produce a result depending on the array type used
+      set v [my $a [dict get? $info {*}$path]]
+    }
+  }
+  
+  method expand {html {extra ""}} {
+    my variable {*}[my types]
+    set info [dict merge $info $extra]
+    Webserver expand $html
+  }
 }
