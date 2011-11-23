17131f8c583415c062c26797b558d87bab8fab17
Jean-Claude Wippler <jc@wippler.nl>
2011-11-23 18:27:32 +0100

    more compact listing, added some comments

--- kit/cmds/doc.tcl
+++ kit/cmds/doc.tcl
@@ -1,19 +1,22 @@
 Jm doc "Print out some documentation extracted from the source code."
 
 proc start {{rig ""} {match *}} {
+  # List all rigs when called without args, else show docs for a specific rig.
   if {$rig eq ""} {
     global auto_index
-    foreach x [lsort -dict [array names auto_index {[A-Za-z]*}]] {
+    # locate all rigs known to the system
+    foreach x [lsort -dict [array names auto_index]] {
       lassign $auto_index($x) cmd path ns
       if {$cmd eq "::Jm::loadRig"} {
-        puts "  $x"
+        # if it really is a rig, load it to obtain the documentation
         Jm loadNow $x
-        puts "    [lindex [dict get? $::Jm::doc_strings $path] 1]"
+        puts "  $x - [lindex [dict get? $::Jm::doc_strings $path] 1]"
       }
     }
   } elseif {[catch { Jm loadNow $rig }]} {
     puts "  $rig - no rig found with this name"
   } else {
+    # Locate all the public procs in a rig, optionally matching a pattern
     foreach x [lsort [info commands "::${rig}::\[a-z]*"]] {
       set args [info args $x]
       set body [info body $x]
@@ -31,12 +34,14 @@ proc start {{rig ""} {match *}} {
       set comments {}
       foreach y [split [string trim $body] "\n"] {
         set line [string trim $y]
+        # only take the first lines which look like a real comment
         if {[string match "# ?*" $line]} {
           lappend comments [string range $line 2 end]
         } elseif {[string index $line 0] ne "#"} {
           break
         }
       }
+      # all data has been collected and formatted, print out the information
       puts ""
       puts "  proc $name {$argsWithDef}"
       if {[llength $comments] > 0} {
