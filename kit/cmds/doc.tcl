9e0bd5b0f3da40e63b0bd281f3f7a482322ad2cb
Jean-Claude Wippler <jc@wippler.nl>
2011-11-23 17:59:34 +0100

    add cmd-line documentation tool

---
+++ kit/cmds/doc.tcl
@@ -0,0 +1,35 @@
+Jm doc "Print out some documentation extracted from the source code."
+
+proc start {rig {match *}} {
+  Jm loadNow $rig
+  foreach x [lsort [info commands "::${rig}::\[a-z]*"]] {
+    set args [info args $x]
+    set body [info body $x]
+    set name [namespace tail $x]
+    if {$match ne "" && ![string match $match $name]} continue
+    # introspect to add the default argument values, if any
+    set argsWithDef {}
+    foreach y $args {
+      if {[info default $x $y v]} {
+        set y [list $y $v]
+      }
+      lappend argsWithDef $y
+    }
+    # extract the first comment lines from the procedure body, if any
+    set comments {}
+    foreach y [split [string trim $body] "\n"] {
+      set line [string trim $y]
+      if {[string match "# ?*" $line]} {
+        lappend comments [string range $line 2 end]
+      } elseif {[string index $line 0] ne "#"} {
+        break
+      }
+    }
+    puts ""
+    puts "  proc $name {$argsWithDef}"
+    if {[llength $comments] > 0} {
+      puts "    [join $comments "\n    "]"
+    }
+  }
+  puts ""
+}
