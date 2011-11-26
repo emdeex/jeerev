545b476ac15b47af74122ef7f653d55d9b3b2595
Jean-Claude Wippler <jc@wippler.nl>
2011-11-26 21:08:20 +0100

    better names

--- examples/driver-demo/main.tcl
+++ examples/driver-demo/main.tcl
@@ -3,10 +3,10 @@ Jm doc "How to decode messages coming from a JeeLink with RF12demo."
 Drivers load ./drivers
 
 # Note: this demo uses a fixed configuration which won't match your setup.
-# To try it, change the device name and the RF12-XXX.YY.Z node IDs as needed.
+# To try it, change the interface name and the RF12-XXX.YY.Z node IDs as needed.
 
 # connected directly via USB
-set device [Config connect:device usb-A700fdxv] ;# or COMn, or /dev/ttyUSBn
+set interface [Config connect:interface usb-A700fdxv] ;# or COMn or /dev/ttyUSBn
 
 # these nodes are picked up from wireless packets
 Drivers register RF12-868.5.2 roomNode
@@ -23,4 +23,4 @@ Drivers register RF12-868.5.24 roomNode
 State subscribe * {apply {x { puts "$x = [State get $x]" }}}
 
 # now we can connect to the JeeLink and start dispatching messages to drivers
-Drivers connect $device autoSketch
+Drivers connect $interface autoSketch
